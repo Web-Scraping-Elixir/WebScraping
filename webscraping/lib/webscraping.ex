7f712b8becc388b010ecef2e390519ab5b993b82
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-06-12 08:16:08 -0300

    FEAT: Adding the get_bloom function using html_parser

--- webscraping/lib/webscraping.ex
+++ webscraping/lib/webscraping.ex
@@ -79,6 +79,45 @@ defmodule Webscraping do
       end
    end
 
+   def get_bloom(busca) do
+      IO.puts("Iniciando busca Bloom")
+
+      case HTTPoison.get("https://www.bloomberg.com/search?query=#{busca}") do
+         {:ok, %HTTPoison.Response{body: body}} ->
+            {:ok, doc} = :html_parser.parse(body)
+
+            content =
+            :html_parser.select(doc, "div.storyItem__aaf871c1c5") |> Enum.map(fn item ->
+               title =
+                  :html_parser.select(item, "a.headline__3a97424275") |> List.first() |> :html_parser.text()
+
+               autor =
+                  :html_parser.select(item, "div.authors__29e96e9287") |> List.first() |> :html_parser.text()
+
+               href =
+                  :html_parser.select(item, "a.headline__3a97424275") |> List.first() |> :html_parser.attribute("href")
+
+               date =
+                  :html_parser.select(item, "div.publishedAt__dc9dff8db4") |> List.first() |> :html_parser.text()
+
+               article =
+                  :html_parser.select(item, "a.summary__a759320e4a") |> List.first() |> :html_parser.text()
+
+               {title, autor, href, date, article}
+            end)
+
+            Enum.each(content, fn {title, autor, href, date, article} ->
+            IO.puts("Title: #{title}")
+            IO.puts("Autor: #{autor}")
+            IO.puts("Link: #{href}")
+            IO.puts("Post info: #{date}")
+            IO.puts("Article: #{article}")
+            IO.puts("")
+            IO.puts("------------------------------------------------------------------------------------------------------------------------")
+            end)
+      end
+   end
+
 end
 
 Webscraping.main()
